d0e677fef41fb33677234f74b0ceb4182f039b8b
nur-frazem <francozanetti302@gmail.com>
2025-12-17 01:32:23 -0300

    Version 1.0.1

--- forge.config.js
+++ forge.config.js
@@ -5,22 +5,15 @@ module.exports = {
   packagerConfig: {
     asar: true,
   },
+  outDir: './out/V1.0.1',
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'linux', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
       config: {},
     },
   ],
